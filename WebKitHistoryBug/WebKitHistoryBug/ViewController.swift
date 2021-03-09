369a8185c5faf3c1e73e31e2e062eceec530832d
Arnold Goldberg <agoldberg@salesforce.com>
2021-03-09 14:00:32 -0600

    Initial Project

---
+++ WebKitHistoryBug/WebKitHistoryBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebKitHistoryBug
+//
+//  Created by Arnold Goldberg on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

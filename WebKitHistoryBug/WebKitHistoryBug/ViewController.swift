6e536811abb307c92339dd6a3e0f3d8b3d6381d0
Arnold Goldberg <agoldberg@salesforce.com>
2021-03-09 15:22:46 -0600

    Add test scenarios: load webview from URL (pointing to a file) load webview using loadFileURL load webview using data load webview using string

--- WebKitHistoryBug/WebKitHistoryBug/ViewController.swift
+++ WebKitHistoryBug/WebKitHistoryBug/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView?
+    @IBOutlet weak var backButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        self.loadURLFromFile()
+    //        self.loadFileURL()
+            self.loadDataURL()
+//            self.loadString()
     }
 
+    @IBAction func goBack(sender: UIButton){
+        webView?.evaluateJavaScript("window.history.back();") { _, _ in
+            
+        }
+    }
+    
+    private func loadURLFromFile() {
+        if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
+            let request = URLRequest(url: testFile)
+            webView?.load(request)
+        }
+    }
+    private func loadFileURL() {
+        if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
+            webView?.loadFileURL(testFile, allowingReadAccessTo: testFile)
+        }
+    }
+
+
+    private func loadDataURL() {
+        if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
+            if let htmlData = try? Data(contentsOf: testFile) {
+                webView?.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: URL(string: "https://www.salesforce.com")!)
+            }
+        }
+    }
+    
+    private func loadString() {
+        if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
+            if let htmlString = try? String(contentsOf: testFile) {
+                webView?.loadHTMLString(htmlString, baseURL: testFile)
+            }
+        }
+    }
 
 }
 

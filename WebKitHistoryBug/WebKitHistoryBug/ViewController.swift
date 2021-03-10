496b798fc907c20543bc6f90e73910744498fa15
Arnold Goldberg <agoldberg@salesforce.com>
2021-03-09 20:38:53 -0600

    Support recreating the webview for different load types.

--- WebKitHistoryBug/WebKitHistoryBug/ViewController.swift
+++ WebKitHistoryBug/WebKitHistoryBug/ViewController.swift
@@ -10,49 +10,45 @@ import WebKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var webView: WKWebView?
+    @IBOutlet weak var containerView: UIView?
     @IBOutlet weak var backButton: UIButton?
-    
+    private var webView: WKWebView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        self.loadURLFromFile()
-    //        self.loadFileURL()
-            self.loadDataURL()
-//            self.loadString()
     }
 
+    private func removeAndRecreateWebView() {
+        webView?.removeFromSuperview()
+        let wv = WKWebView(frame: CGRect(x: 0.0, y: 0.0, width: containerView?.frame.width ?? 0.0, height: containerView?.frame.height ?? 0.0))
+        containerView?.addSubview(wv)
+        self.webView = wv
+    }
+    
     @IBAction func goBack(sender: UIButton){
         webView?.evaluateJavaScript("window.history.back();") { _, _ in
             
         }
     }
     
-    private func loadURLFromFile() {
+    @IBAction func loadURLFromFile() {
+        removeAndRecreateWebView()
         if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
             let request = URLRequest(url: testFile)
             webView?.load(request)
         }
     }
-    private func loadFileURL() {
+    @IBAction func loadFileURL() {
+        removeAndRecreateWebView()
         if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
             webView?.loadFileURL(testFile, allowingReadAccessTo: testFile)
         }
     }
 
-
-    private func loadDataURL() {
-        if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
-            if let htmlData = try? Data(contentsOf: testFile) {
-                webView?.load(htmlData, mimeType: "text/html", characterEncodingName: "UTF-8", baseURL: URL(string: "https://www.salesforce.com")!)
-            }
-        }
-    }
-    
-    private func loadString() {
+    @IBAction func loadString() {
+        removeAndRecreateWebView()
         if let testFile = Bundle.main.url(forResource: "test", withExtension: "html") {
             if let htmlString = try? String(contentsOf: testFile) {
-                webView?.loadHTMLString(htmlString, baseURL: testFile)
+                webView?.loadHTMLString(htmlString, baseURL: URL(string: "https://www.salesforce.com")!)
             }
         }
     }
